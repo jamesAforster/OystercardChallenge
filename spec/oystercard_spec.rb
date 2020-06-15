b791065388b9f188ae9437fba65bf0dd6e18d6b0
Josh <joshua.ng2332@gmail.com>
2020-06-15 16:28:58 +0100

    Added Maximum balance validator

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,5 +15,11 @@ describe Oystercard do
     it "can top up the balance" do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
+
+    it "raises an error when maximum balance is exceeded" do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect { subject.top_up 1 }.to raise_error "£#{maximum_balance} top up limit exceeded!"
+    end
   end
 end
